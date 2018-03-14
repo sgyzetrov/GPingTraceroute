29fb2a5808ad5df356991c3a03408e9dd397059e
Yang (Simon) Guo <34640565+sgyzetrov@users.noreply.github.com>
2018-03-14 13:54:27 +0800

    v0.0.3
    Chinese comments are now written in English

--- GPingTraceroute.c
+++ GPingTraceroute.c
@@ -2,8 +2,8 @@
  * Name: C implementation of ICMP ping and traceroute       
  * Program: GPingTraceroute.c
  * Auther: Guo Yang <guoyang@webmail.hzau.edu.cn>
- * Version: 0.0.2
- * Date(mm/dd/yyyy): 12/19/2017
+ * Version: 0.0.3
+ * Date(mm/dd/yyyy): 3/14/2018
  * Description: 
  *  main function for my own implements of shell command: ping / traceroute
  *  Usage: ./GPingTraceroute -p [hostname/IP address] for ping
@@ -14,7 +14,7 @@
  * 		2. http://blog.csdn.net/joeblackzqq/article/details/41549243
  * 		3. http://www.cnblogs.com/wolflion/p/3345850.html
  * 		4. http://blog.csdn.net/qy532846454/article/details/6915835
- *      5. http://blog.sina.com.cn/s/blog_6a1837e90100uhl3.html 
+ *              5. http://blog.sina.com.cn/s/blog_6a1837e90100uhl3.html 
  * 
  * History:
  *  1. 11/04/2017 guoyang
@@ -25,38 +25,38 @@
 
 # include "./GPingTraceroute.h"
 
-struct hostent * pHost = NULL;		//保存主机信息
-int sock_icmp;						//icmp套接字
+struct hostent * pHost = NULL;		//host info
+int sock_icmp;						//icmp socket
 int nSend = 0;
 int nAttempts = 0;
 char *IP = NULL;
 struct sockaddr_in recv_addr;
-struct sockaddr_in dest_addr; 	//IPv4专用socket地址,保存目的地址
+struct sockaddr_in dest_addr; 	//IPv4 socket address, destination address
 
 int main(int argc, char *argv[])
 {
 	struct protoent *protocol;
 
-	in_addr_t inaddr;				//ip地址（网络字节序）
+	in_addr_t inaddr;				//ip
 
 	if (argc < 3 || (strcmp(argv[1], "-P") != 0 &&
                      strcmp(argv[1], "-p") != 0 &&
                      strcmp(argv[1], "-T") != 0 &&
                      strcmp(argv[1], "-t") != 0 )
-        ) {//处理异常
+        ) {//exception handling
 		printf("Usage: %s -p [hostname/IP address] for ping\n or %s -t [hostname/IP address] for traceroute", argv[0], argv[0]);
 		exit(EXIT_FAILURE);	
 	}
 
-	if ((protocol = getprotobyname("icmp")) == NULL) {//处理异常
+	if ((protocol = getprotobyname("icmp")) == NULL) {//exception handling
 		perror("error in getprotobyname(), unkown protocol");
 		exit(EXIT_FAILURE);
 	}
 
-    // ping的情况
+    // case: ping
     if(!strcmp(argv[1], "-P") || !strcmp(argv[1], "-p")){
-        /* 创建ICMP套接字 */
-        //AF_INET:IPv4, SOCK_RAW:IP协议数据报接口, IPPROTO_ICMP:ICMP协议
+        /* create ICMP socket */
+        //AF_INET:IPv4, SOCK_RAW:IP protocol interface, IPPROTO_ICMP: ICMP protocol
         if ((sock_icmp = socket(AF_INET, SOCK_RAW, protocol->p_proto)) < 0) {
             perror("socket failed, you need root");
             exit(EXIT_FAILURE);
@@ -64,16 +64,16 @@ int main(int argc, char *argv[])
         memset(&dest_addr, 0, sizeof(dest_addr));
         dest_addr.sin_family = AF_INET;
 
-        /* 将点分十进制ip地址转换为网络字节序 */
-        if ((inaddr = inet_addr(argv[2])) == INADDR_NONE) { //转换失败，表明是主机名,需通过主机名获取ip
-            //gethostbyname函数用来向DNS查询一个域名的IP地址
+        /* change ip address's dotted decimal notation to network byte order */
+        if ((inaddr = inet_addr(argv[2])) == INADDR_NONE) { //user input hostname, not ip, need to resolve
+            //gethostbyname() request ip address from DNS
             if ((pHost = gethostbyname(argv[2])) == NULL) {
                 herror("error in gethostbyname(), invalid argument");
                 exit(EXIT_FAILURE);
             }
             memmove(&dest_addr.sin_addr, pHost->h_addr_list[0], pHost->h_length);
         }
-        else {//用户输入的就是IP，那就不用转换了
+        else {//user input ip, no need to resolve
             memmove(&dest_addr.sin_addr, &inaddr, sizeof(struct in_addr));
         }
 
@@ -84,19 +84,19 @@ int main(int argc, char *argv[])
 
         IP = argv[2];
         signal(SIGINT, final_statistics_print);
-        while (nSend < MAX_PACKETS_NUM_PER_PING) { //一共发送MAX_PACKETS_NUM_PER_PING次报文
+        while (nSend < MAX_PACKETS_NUM_PER_PING) { //MAX_PACKETS_NUM_PER_PING num of datagram
             int unpack_ret;
             send_packet(sock_icmp, &dest_addr, nSend);
             unpack_ret = receive_packet_Ping(sock_icmp);//, &dest_addr);
-            if (-1 == unpack_ret)	//（ping回环时）收到了自己发出的报文,重新等待接收
+            if (-1 == unpack_ret)	//received own datagram, waiting for new income
                 receive_packet_Ping(sock_icmp);//, &dest_addr);
             sleep(1);
             nSend++;
         }
-        final_statistics_print(0);	//输出统计信息
+        final_statistics_print(0);	//print statistics
     }
 
-    // traceroute的情况
+    // case: traceroute
     else if(!strcmp(argv[1], "-T") || !strcmp(argv[1], "-t")){
         int i, unpack_ret_traceroute, ttl;
         int nSend_traceroute = 0;
@@ -114,16 +114,16 @@ int main(int argc, char *argv[])
             memset(&dest_addr, 0, sizeof(dest_addr));
             dest_addr.sin_family = AF_INET;
             
-            /* 将点分十进制ip地址转换为网络字节序 */
-            if ((inaddr = inet_addr(argv[2])) == INADDR_NONE) { //转换失败，表明是主机名,需通过主机名获取ip
-                //gethostbyname函数用来向DNS查询一个域名的IP地址
+            /* change ip address's dotted decimal notation to network byte order */
+            if ((inaddr = inet_addr(argv[2])) == INADDR_NONE) { //user input hostname, not ip, need to resolve
+                //gethostbyname() request ip address from DNS
                 if ((pHost = gethostbyname(argv[2])) == NULL) {
                     herror("error in gethostbyname(), invalid argument");
                     exit(EXIT_FAILURE);
                 }
                 memmove(&dest_addr.sin_addr, pHost->h_addr_list[0], pHost->h_length);
             }
-            else {//用户输入的就是IP，那就不用转换了
+            else {//user input ip, no need to resolve
                 memmove(&dest_addr.sin_addr, &inaddr, sizeof(struct in_addr));
             }
 
